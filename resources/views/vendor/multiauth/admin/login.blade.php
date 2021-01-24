1986a32d4d29ec29e4951fb6ed3603a2591d146c
SakilAhmed007 <sakilhossain01969@gmail.com>
2021-01-24 09:29:09 +0600

    changed login blade

--- resources/views/vendor/multiauth/admin/login.blade.php
+++ resources/views/vendor/multiauth/admin/login.blade.php
@@ -1,66 +1,91 @@
-@extends('multiauth::layouts.app') 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ ucfirst(config('multiauth.prefix')) }} Login</div>
+<!DOCTYPE html>
+<html lang="en">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('admin.login') }}" aria-label="{{ __('Admin Login') }}">
-                        @csrf
+<head>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+	<title>Flat Able - Premium Admin Template by Phoenixcoded</title>
+	<!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 11]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="">
+	<meta name="author" content="Phoenixcoded" />
+	<!-- Favicon icon -->
+	<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
+	<!-- vendor css -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+	
+	
+
+
+</head>
+
+<!-- [ auth-signin ] start -->
+<div class="auth-wrapper">
+	<div class="auth-content text-center">
+		<img src="assets/images/logo.png" alt="" class="img-fluid mb-4">
+		<div class="card borderless">
+			<div class="row align-items-center ">
+				<div class="col-md-12">
+					<div class="card-body">
+						<h4 class="mb-3 f-w-400">Signin</h4>
+						<hr>
+						
+						<form method="POST" action="{{ route('admin.login') }}">
+							@csrf
+
+							<div class="form-group mb-3">
+								 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
                                     required autofocus> @if ($errors->has('email'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span> @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+							</div>
+							<div class="form-group mb-4">
+								<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
                                     required> @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span> @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
+							</div>
+							<div class="custom-control custom-checkbox text-left mb-4 mt-2">
+								{{-- <input type="checkbox" class="custom-control-input" id="customCheck1">
+								<label class="custom-control-label" for="customCheck1">Save credentials.</label> --}}
+								<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
-                                </div>
-                            </div>
-                        </div>
+							</div>
+							<button type="submit" class="btn btn-block btn-primary mb-4">Signin</button>
+							<hr>
+							<p class="mb-2 text-muted">Forgot password? <a href="{{ route('admin.password.request') }}" class="f-w-400">Reset</a></p>
+						</form>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('admin.password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-@endsection
+<!-- [ auth-signin ] end -->
+
+<!-- Required Js -->
+<script src="{{ asset('backend/assets/js/vendor-all.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/plugins/bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('backend/assets/js/pcoded.min.js') }}"></script>
+
+
+
+</body>
+
+</html>
